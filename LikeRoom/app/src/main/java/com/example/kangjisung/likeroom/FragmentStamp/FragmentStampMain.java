8f5a9f01580177b794a7ca142bd19c1121da0f88
stories2 <stories2@naver.com>
2016-12-25 11:53:33 +0900

    쿠폰과 스탬프 전환버튼 추가

--- LikeRoom/app/src/main/java/com/example/kangjisung/likeroom/FragmentStamp/FragmentStampMain.java
+++ LikeRoom/app/src/main/java/com/example/kangjisung/likeroom/FragmentStamp/FragmentStampMain.java
@@ -1,19 +1,36 @@
 package com.example.kangjisung.likeroom.FragmentStamp;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.kangjisung.likeroom.R;
 
 public class FragmentStampMain extends Fragment {
 
+    //스탬프 레이아웃에서 선택된 매장에서 현제 나의 스탬프 상황을 봄
+    View stampLayout;
+    Button btnShowSpecialStamp;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         /* 초기화 코드는 여기서 */
-        return inflater.inflate(R.layout.fragment_stamp_main, container, false);
+        stampLayout = inflater.inflate(R.layout.fragment_stamp_main, container, false);
+        btnShowSpecialStamp = (Button)stampLayout.findViewById(R.id.btnShowSpecialStamp);
+
+        //쿠폰<->스탬프 레이아웃을 전환하면서 나의 쿠폰과 스탬프 상태를 봄
+        btnShowSpecialStamp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, getString(R.string.featureLoadFail), Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        return stampLayout;
     }
 
     /* 이벤트 코드는 여기서 */
