7ae21159a9956236a16a4e62b4758adb55868d2a
stories2 <stories2@naver.com>
2016-12-26 14:04:15 +0900

    공지 예시, 햄버거 이미지 삭제

--- LikeRoom/app/src/main/java/com/example/kangjisung/likeroom/FragmentNotice/FragmentNoticeMain.java
+++ LikeRoom/app/src/main/java/com/example/kangjisung/likeroom/FragmentNotice/FragmentNoticeMain.java
@@ -7,12 +7,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.kangjisung.likeroom.DefineManager;
 import com.example.kangjisung.likeroom.R;
-
-import java.util.GregorianCalendar;
 
 public class FragmentNoticeMain extends Fragment {
 //스탬프,공지사항,매장정보 중 공지사항 부분.
@@ -22,6 +21,7 @@ public class FragmentNoticeMain extends Fragment {
     private NoticeRecyclerViewAdapter mAdapter;
     String[] selectedShopInfoData;
     TextView txtStoreName;
+    ImageView hamburgerMenu;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,16 +31,19 @@ public class FragmentNoticeMain extends Fragment {
         mLayoutManager = new LinearLayoutManager(getActivity());
         txtStoreName = (TextView) view.findViewById(R.id.txtStoreName);
         noticeRecyclerView = (RecyclerView) view.findViewById((R.id.recyclerView));
+        hamburgerMenu = (ImageView) view.findViewById(R.id.hamburgerMenu);
 
+        hamburgerMenu.setVisibility(View.INVISIBLE);
         selectedShopInfoData = getArguments().getStringArray("shopInfoData");
 
         noticeRecyclerView.setAdapter(mAdapter);
         noticeRecyclerView.setLayoutManager(mLayoutManager);
         txtStoreName.setText(selectedShopInfoData[DefineManager.shopNameSavedPoint]);
 
-        mAdapter.addItem("제목1", "내용1", new GregorianCalendar(2016, 1, 1), new GregorianCalendar(2016, 12, 30), 1);
+        /*mAdapter.addItem("제목1", "내용1", new GregorianCalendar(2016, 1, 1), new GregorianCalendar(2016, 12, 30), 1);
         mAdapter.addItem("제목2", "내용2", new GregorianCalendar(2015, 1, 1), new GregorianCalendar(2015, 12, 30), 2);
-        mAdapter.addItem("제목3", "내용3", new GregorianCalendar(2014, 1, 1), new GregorianCalendar(2014, 12, 30), 3);
+        mAdapter.addItem("제목3", "내용3", new GregorianCalendar(2014, 1, 1), new GregorianCalendar(2014, 12, 30), 3);*/
+
 
         return view;
     }
