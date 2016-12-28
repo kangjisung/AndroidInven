11b426ecd52d157322615c3a19b1ed5f391d3353
stories2 <stories2@naver.com>
2016-12-28 13:26:57 +0900

    머지 취소
    This reverts commit 4111b49234d5966ad7b59d05a66087dba27dc0c5, reversing
    changes made to 5014cb8852b89f03c3900d6f9c6629bea90781ec.

---
+++ LikeRoom/app/src/main/java/com/example/kangjisung/likeroom/FragmentItemMain.java
@@ -0,0 +1,18 @@
+package com.example.kangjisung.likeroom;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentItemMain extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /* 초기화 코드는 여기서 */
+        return inflater.inflate(R.layout.fragment_item_main, container, false);
+    }
+
+    /* 이벤트 코드는 여기서 */
+}
