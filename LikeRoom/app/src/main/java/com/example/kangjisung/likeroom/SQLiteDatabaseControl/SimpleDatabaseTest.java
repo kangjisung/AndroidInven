b8022ec46118ab78314c6a71d0f2f098ecc71164
ljh97 <you9182@naver.com>
2017-01-15 20:45:46 +0900

    클라이언트DB 구현
    초기단계

--- LikeRoom/app/src/main/java/com/example/kangjisung/likeroom/SQLiteDatabaseControl/SimpleDatabaseTest.java
+++ LikeRoom/app/src/main/java/com/example/kangjisung/likeroom/SQLiteDatabaseControl/SimpleDatabaseTest.java
@@ -5,11 +5,13 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+import com.example.kangjisung.likeroom.MainActivity;
 import com.example.kangjisung.likeroom.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static android.R.attr.max;
 import static com.example.kangjisung.likeroom.DefineManager.customerDatabaseName;
 import static com.example.kangjisung.likeroom.DefineManager.databaseShopAddressSavedPoint;
 import static com.example.kangjisung.likeroom.DefineManager.databaseShopNameSavedPoint;
@@ -20,22 +22,23 @@ import static com.example.kangjisung.likeroom.DefineManager.databaseShopPhoneNum
  */
 
 public class SimpleDatabaseTest {
-    final int storeIdSavedPoint = 0, isStoreRegisteredToMe = 8;
+    final int storeIdSavedPoint = 0, isStoreRegisteredToMe = 8; // 코드 가독성을 위해서
     Context context;
     LocalHostDatabaseManager localHostDatabaseManager;
     SQLiteDatabase sqLiteDatabase;
 
+    //테스트
     String[][] allRegisteredStoreInfo = new String[][]{
-            {"" + 1, "고덕리엔파크점", "서울특별시 강동구 상일동 75-2", "024268758", "N/A", "N/A", "" + 37.5537472, "" + 127.1716884, "" + 1},
+            /*{"" + 1, "고덕리엔파크점", "서울특별시 강동구 상일동 75-2", "024268758", "N/A", "N/A", "" + 37.5537472, "" + 127.1716884, "" + 1},
             {"" + 2, "상일세종점", "서울특별시 강동구 상일동 67-2", "024417833", "N/A", "N/A", "" + 37.5509792, "" + 127.1738538, "" + 1},
-            {"" + 3, "전화로 세운 건물", "콜로세움", "000000000", "08:30", "15:30", "" + 41.8902102, "" + 12.4922309, "" + 1},
+            {"" + 3, "전화로 세운 건물", "콜로세움", "000000000", "08:30", "15:30", "" + 41.8902102, "" + 12.4922309, "" + 1},*/
     };
     String[][] eachStoreNoticeInfo = new String[][]{
-        {"" + 1, "" + 1, "hello world", "this is test", "2015 2 1", "2016 1 30"},
+     /* {"" + 1, "" + 1, "hello world", "this is test", "2015 2 1", "2016 1 30"},
         {"" + 1, "" + 2, "hi", "can u see me?", "2016 2 22", "2017 8 19"},
         {"" + 1, "" + 2, "max length test aaaaaaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "2016 2 22", "2017 8 19"},
         {"" + 2, "" + 3, "test", "test notice", "2016 1 1", "2017 1 1"},
-        {"" + 3, "" + 4, "콜로세움 공지", "빵 안팔아", "0080 1 1", "9999 1 1"},
+        {"" + 3, "" + 4, "콜로세움 공지", "빵 안팔아", "0080 1 1", "9999 1 1"},*/
     };
 
     public SimpleDatabaseTest() {
@@ -45,7 +48,6 @@ public class SimpleDatabaseTest {
     public SimpleDatabaseTest(Context context) {
         this.context = context;
         localHostDatabaseManager = new LocalHostDatabaseManager(context, context.getApplicationInfo().dataDir + "/databases/", customerDatabaseName);
-
         sqLiteDatabase = localHostDatabaseManager.OpenSQLiteDatabase();
         Cursor sqlResult = sqLiteDatabase.rawQuery("select * from `매장`;", null);
         while(sqlResult.moveToNext()) {
@@ -54,8 +56,17 @@ public class SimpleDatabaseTest {
             Log.d(context.getString(R.string.app_name), "data: " + storeId + " " + storeName);
         }
     }
-
+    // all에 한 열 그거를 전체를 다 인덱스에 대입 인덱스에 store번지값을 비교를해서(타겟스토어 넘버:찾고자 하는 스토어) 넘버 맞으면 리턴을한다.
     public String[] GetSelectedStoreInfo(int targetStoreNumber) {
+        this.context = context;
+        localHostDatabaseManager = new LocalHostDatabaseManager(context, context.getApplicationInfo().dataDir+ "/databases/", customerDatabaseName);
+        sqLiteDatabase = localHostDatabaseManager.OpenSQLiteDatabase();
+        Cursor sqlResult = sqLiteDatabase.rawQuery("select * from '매장';", null);
+
+        while(sqlResult.moveToNext()){
+            String st
+        }
+
         for (String[] indexOfStoreInfo: allRegisteredStoreInfo) {
             if(indexOfStoreInfo[storeIdSavedPoint].equals("" + targetStoreNumber)) {
                 return indexOfStoreInfo;
