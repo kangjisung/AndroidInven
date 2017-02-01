8d741d75d3844fe651cf5910a5ebea7321cc3c37
stories2 <stories2@naver.com>
2017-02-01 13:30:48 +0900

    서버에 매장을 등록하는 메소드 추가

--- StoreApp/app/src/main/java/com/example/kangjisung/likeroom/CommunicationManager/NetWorkOrderProcessManager.java
+++ StoreApp/app/src/main/java/com/example/kangjisung/likeroom/CommunicationManager/NetWorkOrderProcessManager.java
@@ -4,6 +4,9 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by stories2 on 2017. 1. 24..
  */
@@ -55,5 +58,32 @@ public class NetWorkOrderProcessManager {
         catch (Exception err) {
             Log.d(logCatTag, "Error in LoadAllStoreInfo: " + err.getMessage());
         }
+    }
+
+    public boolean InsertNewStoreInfoData(String storeName, String storePhoneNumber, String introduceString, String storeAddress, String openTime,
+                                       String closeTime, double storePlaceLatitude, double storePlaceLongtitude, String storeCountryCode){
+        /*address=<매장 주소>&latitude=<매장 위도>&longtitude=<매장 경도>&name=<매장 이름>&phone=<매장 전화번호>&introduce=<매장 소개글>
+        &countryCode=<매장 국가 코드>&serivceRegisterDate=<매장 등록 날짜>&updateInfoDate=<매장 정보 마지막 수정 날짜>&openTime=<매장 개장 시간>
+        closeTime=<매장 마감 시간>&disable=<서비스 탈퇴 여부>*/
+        networkModule = new NetworkModule();
+        String responseRawData = null, serverStatus = null;
+        Date storeRegisteredDate = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            responseRawData = networkModule.execute("http://" + hostName + apiName + "/InsertNewStoreInfoData/?address=" + storeAddress +
+                    "&latitude=" + storePlaceLatitude + "&longtitude=" + storePlaceLongtitude + "&name=" + storeName +
+                    "&phone=" + storePhoneNumber + "&introduce=" + introduceString + "&countryCode=" + storeCountryCode +
+                    "&serivceRegisterDate=" + simpleDateFormat.format(storeRegisteredDate) + "&updateInfoDate=" + simpleDateFormat.format(storeRegisteredDate) +
+                    "&openTime=" + openTime + "closeTime=" + closeTime).get();
+            JSONObject jsonObject = new JSONObject(responseRawData);
+            serverStatus = jsonObject.getString("Result");
+            if(serverStatus.equals("Ok")) {
+                return true;
+            }
+        }
+        catch (Exception err) {
+            Log.d(logCatTag, "Error in InsertNewStoreInfoData: " + err.getMessage());
+        }
+        return false;
     }
 }
