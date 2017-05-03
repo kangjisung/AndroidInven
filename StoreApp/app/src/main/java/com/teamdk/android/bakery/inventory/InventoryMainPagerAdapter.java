980e4eacdcf01a9797a5a9e5151b55b67388d3d4
Dinsslug <dinsslug@gmail.com>
2017-05-03 20:34:44 +0900

    프래그먼트3 삭제

--- StoreApp/app/src/main/java/com/teamdk/android/bakery/inventory/InventoryMainPagerAdapter.java
+++ StoreApp/app/src/main/java/com/teamdk/android/bakery/inventory/InventoryMainPagerAdapter.java
@@ -16,17 +16,16 @@ public class InventoryMainPagerAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        if (position == 0)
+        if (position == 0) {
             return new Fragment1();
-        else if(position==1)
-            return new Fragment2();
+        }
         else {
-            return new Fragment3();
+            return new Fragment2();
         }
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return 2;
     }
 }
